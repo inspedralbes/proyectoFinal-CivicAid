05a05656c7e56cac1f278f9a324201c01c120261
CarlosGomezFuentes <a21cargomfue@inspedralbes.cat>
2024-02-12 22:37:34 +0100

    Estilos de la LandingPage modificados

--- Frontend/my-app/src/components/provider_home.js
+++ Frontend/my-app/src/components/provider_home.js
@@ -3,6 +3,13 @@ import { useDispatch } from 'react-redux';
 import { store, actions } from './store';
 import { NavLink } from 'react-router-dom';
 import Swal from "sweetalert2";
+import {
+    Carousel,
+    initTE,
+} from "tw-elements";
+
+initTE({ Carousel });
+
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -21,69 +28,53 @@ const Home = () => {
     }
 
     return (
-        <main className='min-h-screen bg-gray-100 items-center justify-center'>
-            <div className='text-center pt-5'>
-                <h1 className='text-5xl font-bold mb-4'>CivicAid</h1>
-            </div>
+        <main className='text-center pt-5'>
 
-            <div className='bg-white p-8 rounded shadow-md max-w-3xl w-full'>
+            <h1 className='text-5xl font-bold mb-4 bg-slate-600'>CivicAid</h1>
 
-                <div className='text-center'>
+            <initTE></initTE>
+            <div className='bg-white p-8 rounded shadow-md max-w-3xl mx-auto'>
+
+                <div className='mb-4'>
                     <NavLink to="/makeApplication">
-                        <button
-                            className='bg-blue-500 text-white p-2 rounded hover:bg-blue-700'
-                        >
+                        <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 w-full'>
                             HACER SOLICITUD
                         </button>
                     </NavLink>
                 </div>
 
-                <br />
-
-                <div className='text-center'>
+                <div className='mb-4'>
                     <NavLink to="/login">
-                        <button
-                            className='bg-blue-500 text-white p-2 rounded hover:bg-blue-700'
-                        >
-                            MODO CIUIDADANO
+                        <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 w-full'>
+                            MODO CIUDADANO
                         </button>
                     </NavLink>
                 </div>
 
-                <br />
-
-                <div className='text-center'>
+                <div className='mb-4'>
                     <NavLink to="/loginWorker">
-                        <button
-                            className='bg-blue-500 text-white p-2 rounded hover:bg-blue-700'
-                        >
+                        <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 w-full'>
                             MODO PROFESIONAL
                         </button>
                     </NavLink>
                 </div>
 
-                <br />
-
-                <div className='text-center'>
-                    <button
-                        onClick={() => logout()} className='bg-blue-500 text-white p-2 rounded hover:bg-blue-700'
-                    >
+                <div className='mb-4'>
+                    <button onClick={() => logout()} className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 w-full'>
                         LOGOUT
                     </button>
                 </div>
+
             </div>
 
-            <div>
-                <div className='text-center'>
-                    <NavLink to="/profile">
-                        <button
-                            className='bg-blue-500 text-white p-2 rounded hover:bg-blue-700'
-                        >
-                            PERFIL
-                        </button>
-                    </NavLink>
-                </div>
+            <div className='mt-4'>
+                <NavLink to="/profile">
+                    <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'>
+                        PERFIL
+                    </button>
+                </NavLink>
             </div>
+
         </main>
     );
 }
